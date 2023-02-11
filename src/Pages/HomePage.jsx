74029b8ff7641cbbf678bbe5410b84b1f091d331
Abhishek <abhiofficial581@gmail.com>
2023-02-11 11:59:02 +0530

    till now

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/HomPageComponents/Header'
+const HomePage = () => {
+  return (
+    <div id='homePage-container'>
+      <Header/>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/HomPageComponents/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar'
+import './Header.css'
+const Header = () => {
+  return (
+    <div id='header-container'>
+      <NavBar/>
+      <div id="contentInHeader-container">
+        hello
+      </div>
+    </div>
+  )
+}
+
+export default Header
