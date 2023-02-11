74c5e556f6e7d5b50a0c8ce9c4dcc9e96d7b0bcf
Abhishek <abhiofficial581@gmail.com>
2023-02-11 14:39:47 +0530

    header

--- src/components/HomPageComponents/Header.jsx
+++ src/components/HomPageComponents/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NavBar from './NavBar'
 import './Header.css'
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 const Header = () => {
   return (
     <div id='header-container'>
       <NavBar/>
       <div id="contentInHeader-container">
-        hello
+        <h1>Carbon Action Solution for <br></br> Internet Data Transfer</h1>
+        <div id="videoBtnHeader">
+            <button><PlayCircleIcon id="videoPlayIcon"/></button>
+        </div>
+        <h5>The CarbonInsight shows the ranking of websites and users who installed our extension.<br></br> This extension shows the carbon footprint , user produce by visiting websites.</h5>
       </div>
     </div>
   )
