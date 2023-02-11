c7882e2ed8347a3dbf1da959e611a57238c79a0f
Abhishek <abhiofficial581@gmail.com>
2023-02-11 14:45:41 +0530

    ExtensionIcon

--- src/components/HomPageComponents/Header.jsx
+++ src/components/HomPageComponents/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './NavBar'
 import './Header.css'
-import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import ExtensionIcon from '@mui/icons-material/Extension';
 const Header = () => {
   return (
     <div id='header-container'>
@@ -9,7 +9,7 @@ const Header = () => {
       <div id="contentInHeader-container">
         <h1>Carbon Action Solution for <br></br> Internet Data Transfer</h1>
         <div id="videoBtnHeader">
-            <button><PlayCircleIcon id="videoPlayIcon"/></button>
+            <button><ExtensionIcon id="videoPlayIcon"/></button>
         </div>
         <h5>The CarbonInsight shows the ranking of websites and users who installed our extension.<br></br> This extension shows the carbon footprint , user produce by visiting websites.</h5>
       </div>
