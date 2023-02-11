86514f4a41b0e310bdd2e8256e7c30250c7db9a0
Abhishek <abhiofficial581@gmail.com>
2023-02-11 16:54:28 +0530

    Contact Footer

--- src/components/HomPageComponents/Contact.jsx
+++ src/components/HomPageComponents/Contact.jsx
@@ -9,13 +9,32 @@ const Contact = () => {
     <div id='contact-container'>
       <div id="contactName"><h3>CarBon</h3><h3>Insight</h3></div>
       <div id='contact-list'>
-        <h6>Social Media</h6>
-        <div id="mediaHandles">
-            <WhatsAppIcon className='handles'/>
-            <TwitterIcon className='handles'/>  
-            <LinkedInIcon className='handles'/>  
-            <FacebookIcon className='handles'/>  
-
+        <div className="Lists">
+            <div id="mediaHandles">
+                <WhatsAppIcon className='handles'/>
+                <TwitterIcon className='handles'/>  
+                <LinkedInIcon className='handles'/>  
+                <FacebookIcon className='handles'/>  
+            </div>
+        </div>
+        <div className="Lists">
+            <h5>Features</h5>
+            <h6>Calculating Carbon Footprints</h6>
+            <h6>Shows Ranking</h6>
+            <h6>User monthly activity</h6>
+            <h6>Recommend sites with less carbon equivalent</h6>
+        </div>
+        <div className="Lists">
+            <h5>Conducted By</h5>
+            <h6>NIT Tiruchirappalli</h6>
+            <h6>NIT Warangle</h6>
+            <h6>NIT Karnataka</h6>
+        </div>
+        <div className="Lists">
+            <h5>Referances</h5>
+            <h6><a href="https://www.bbc.com/future/article/20200305-why-your-internet-habits-are-not">BBC</a></h6>
+            <h6><a href="https://sphera.com/blog/the-carbon-footprint-of-the-internet/">Sphera</a></h6>
+            <h6><a href="https://sustainablewebdesign.org/calculating-digital-emissions/">Sustainablewebdesign</a></h6>
         </div>
       </div>
     </div>
